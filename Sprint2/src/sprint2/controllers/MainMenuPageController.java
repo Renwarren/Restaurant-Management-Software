63b3328e9ce322a83b1a9a12c9379c4bbff74743
Warren Fongang <70856761+Renwarren@users.noreply.github.com>
2022-11-25 17:36:27 -0500

    Main Menu Page upload
    - Designed main menu page
    - Created main menu page controller

---
+++ Sprint2/src/sprint2/controllers/MainMenuPageController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprint2.controllers;
+
+/**
+ *
+ * @author warren
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuPageController {
+
+    @FXML
+    private Button floorButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button orderButton;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    void loadOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadStore(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+}
